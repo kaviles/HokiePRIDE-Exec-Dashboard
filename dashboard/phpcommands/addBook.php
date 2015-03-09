3d7af7934528c2eff02cd52244d15bd025b404bc
Kelvin Aviles <kaviles@vt.edu>
2015-03-09 03:19:48 -0400

    Corrections in addBook.php
    Made some corrections for the _REQUEST calls. Changed the names for
    some of the values in the bookData associative array.

--- dashboard/phpcommands/addBook.php
+++ dashboard/phpcommands/addBook.php
@@ -4,10 +4,9 @@ include_once('dashcommands.php');
 header('Content-Type: application/json');
 
 $bookData = array("title"=>$_REQUEST['title'], "author"=>$_REQUEST['author'],
-	"pub"=>$_REQUEST['pub'], "year"=>$_REQUEST['year'], "isbn"=>$_REQUEST['isbn'],
-	"loc"=>$_REQUEST[''], "dcc"=>$_REQUEST['dcc'], "tags"=>$_REQUEST['tags'],
-	"covurl"=>$_REQUEST['covurl'], "comms"=>$_REQUEST['comms'], "libid"=>$_REQUEST['libid']);
-
+	"pub"=>$_REQUEST['pub'], "year"=>$_REQUEST['year'], "isbn13"=>$_REQUEST['isbn13'],
+	"loc"=>$_REQUEST['loc'], "dcc"=>$_REQUEST['dcc'], "tags"=>$_REQUEST['tags'],
+	"covurl"=>$_REQUEST['covurl'], "comms"=>$_REQUEST['desc'], "libid"=>$_REQUEST['libid']);
 
 $response = addBook($bookData);
 echo json_encode($response);

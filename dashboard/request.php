10a1806ea536d22ef8001a0fc3f4ce7c5d337695
Kelvin Aviles <kaviles@vt.edu>
2016-01-11 02:01:57 -0500

    Changed authentication methods.
    Request will now determine if user is an admin or public user and
    populate an associative a array with valid commands and their
    corresponding files which is then used to authorize access to certain
    api commands.

--- dashboard/request.php
+++ dashboard/request.php
@@ -1,30 +1,80 @@
 <?php
 
-include_once(__DIR__.'/includes/utility.php');
+// if (!isset($_SERVER['HTTPS'])) {
+//     $response = '{"responseCode":0, "message":"Request must be over HTTPS."}';
+//     echo json_encode($response);
+//     die();
+// }
+
+if (!isset($_GET['requestType']) || empty($_GET['requestType'])) {
+    $response = '{"responseCode":0, "message":"requestType parameter must be set."}';
+    echo json_encode($response);
+    die();
+}
 
 header('Content-Type: application/json');
 
-// $req_params = json_decode(implode("", $_GET), true);
-$req_params = $_GET;
+include_once(__DIR__.'/includes/utility.php');
 
-$requestType = $req_params['requestType'];
+// cas_setup();
 
-logMessage(__DIR__."/logs/request.log", "Request: ".$_SERVER['REQUEST_URI']);
+// if (cas_isAuthenticated()) {
 
-$removeArray = array("*", "'", "\"", "\\", "/", ".", ",");
-$requestTypeSafe = str_replace($removeArray, "", $requestType);
+//     $user = cas_getUser();
+//     if (authorizeAdmin($user)) {
+//         $requestFromAdmin = true;
+//     }
+//     else {
+//         $requestFromAdmin = false;
+//     }
+// }
+// else {
+//     $user = "public";
+//     $requestFromAdmin = false;
+// }
 
-$file = "api/".$requestTypeSafe.".php";
+$user = "testAdmin";
+$requestFromAdmin = true;
 
-$response = '';
-if (file_exists($file))
-{
-    include_once($file);
-    $requestData = $req_params['requestData'];
-    $response = handleRequestData($requestData);
+if ($requestFromAdmin) {
+    $apiArray = array(
+        "addAdmin" => "addAdmin.php", "addBook" => "addBook.php", "addPatron" => "addPatron.php",
+        "checkInBook" => "checkInBook.php", "checkOutBook" => "checkOutBook.php",
+        "deleteAdmin" => "deleteAdmin.php", "deleteBook" => "deleteBook.php", "deletePatron" => "deletePatron.php",
+        "editBook" => "editBook.php", "editPatron" => "editPatron.php",
+        "generateLibID" => "generateLibID.php",
+        "getAdmin" => "getAdmin.php","getBook" => "getBook.php", "getPatron" => "getPatron.php",
+        "removeBook" => "removeBook.php", "removePatron" => "removePatron.php",
+        "retrieveBookData" => "retrieveBookData.php", "searchRequest" => "searchRequestAdmin.php"
+    );
+
+    logMessage(__DIR__."/logs/request.log", "Admin Request [".$user."]: ".$_SERVER['REQUEST_URI']);
 }
 else {
-    $response = '{"responseCode":"0","message":"Invalid request command '.$requestTypeSafe.'"}';
+    $apiArray = array(
+        "searchRequest" => "searchRequestPublic.php"
+    );
+
+    logMessage(__DIR__."/logs/request.log", "Public Request, [".$user."]: ".$_SERVER['REQUEST_URI']);
+}
+
+$requestType = $_GET['requestType'];
+$requestData = $_GET['requestData'];
+
+if (array_key_exists($requestType, $apiArray)) {
+
+    $file = __DIR__."/api/".$apiArray[$requestType];
+
+    if (file_exists($file)) {
+        include_once($file);
+        $response = handleRequestData($requestData);
+    }
+    else {
+        $response = '{"responseCode":2,"message":"Error accessing api: '.$requestType.'"}';
+    }
+}
+else {
+    $response = '{"responseCode":0,"message":"Invalid api request: '.$requestType.'"}';
 }
 
 logMessage(__DIR__."/logs/request.log", "Response: ".$response);
